eb7d4918674e2a42c53f1cb917ed6495beb5e602
Sean Andrew Wiesner <saw336@ellis-compute-01.cs.cornell.edu>
2023-11-28 17:39:38 -0500

    successfully formatted inputs for humanEval

--- generate_postconditions.py
+++ generate_postconditions.py
@@ -9,7 +9,7 @@ INPUT_INTRO = "Given the problem description of the programming problem defined 
 
 POSTCONDITION_DEF = "Definition of a post-condition: A post-condition is an assert statement that checks for a condition that should be true regardless of the input.\n\n"
 
-EXAMPLE_POSTCONDITION = "Here is an example post-condition for an arbitrary programming problem:\n'# Post-condition 1: The output should be a float or an integer.\nassert isinstance(result, (int, float))'"
+EXAMPLE_POSTCONDITION = 'Here is an example post-condition for an arbitrary programming problem:\n"# Post-condition 1: The output should be a float or an integer.\nassert isinstance(result, (int, float))"'
 
 
 def load_data(dataset):
@@ -27,35 +27,44 @@ def load_data(dataset):
 
 def format_inputs(raw_data, dataset):
 
+    prompt_data = []
     if dataset == "humanEval":
-        prompt_data = []
         for prompt in raw_data['prompt']:
             prompt = prompt.split(">>>", 1)[0].strip(" ").strip("\n")
             prompt = prompt.replace("\n", "")
             if '"""' in prompt:
-                prompt = prompt.split('"""', 1)
+                prompt = prompt.split('"""')
             else:
-                prompt = prompt.split("'''", 1)
+                prompt = prompt.split("'''")
 
             function_def = prompt[0].strip(" ")
-            function_desc = prompt[1].strip(" ")
+            function_desc = prompt[1].split("Example:")[0].strip(" ")
 
             def_idx = function_def.find("def")
             if def_idx != 0:
                 function_def = function_def[:def_idx] + \
                     "\n" + function_def[def_idx:]
+            function_def = function_def.strip("\n")
 
-            prompt = function_def + "\n" + function_desc
+            prompt = '"' + function_def + "\n" + function_desc + '"\n\n'
+            prompt = INPUT_INTRO + 'Programming problem:\n' + \
+                prompt + POSTCONDITION_DEF + EXAMPLE_POSTCONDITION
             prompt_data.append(prompt)
 
-        return prompt_data[0]
+    else:
+        pass
+
+    return prompt_data
 
 
 def main():
     dataset = 'humanEval'
     raw_data = load_data(dataset)
     formatted_inputs = format_inputs(raw_data, dataset)
-    print(formatted_inputs)
+
+    with open('generated_inputs.txt', 'w') as f:
+        for generation in formatted_inputs:
+            f.write(f"{generation}\n\n********\n\n")
 
 
 if __name__ == "__main__":
